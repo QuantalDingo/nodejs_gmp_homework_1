939c227078697ec242b1fad0d82bcc0990162bcc
Dmitry Kovalkov <dmkovalkov@air-dmitry.lan>
2021-10-11 16:30:31 +0300

    feat(): Add error handling for task 1.1

--- src/task1.ts
+++ src/task1.ts
@@ -6,6 +6,8 @@ stdin.on('data', (data: string) => {
 	stdout.write(parseInputData(data) + '\n\n', 'utf-8');
 });
 
+stdin.on('error', (err) => console.error(err.message));
+
 function parseInputData(data: string): string {
-	return data.trimEnd().split('').reverse().join('');
+	return data.trim().split('').reverse().join('');
 }
